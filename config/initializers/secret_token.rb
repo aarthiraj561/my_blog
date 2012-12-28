89ea09f1d7120578a2ef71f56074ef2aa9006149
aarthi <aarthi@dell.(none)>
2012-12-28 23:48:36 +0530

    add empty framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'da083fd4a4f2bfa8bd4f50d744b1d20bbd554ce4e72f3f06ac1cf8dbefd0a3d6cbfa6de9301dd091805cd21e9415f63c6e1e055be57d0880db8fd2c592307598'
